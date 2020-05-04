b6ff4eca7094b03d98ad088c766771c8d6529671
Noriaki <sepiano15@gmail.com>
2020-05-04 17:14:12 +0900

    Add Page.txt : A couple of pages we have to make.

--- FindLove/settings.py
+++ FindLove/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -107,6 +108,7 @@ DATABASES = {
         'HOST': 'Noriaki.mysql.pythonanywhere-services.com',
     }
 }
+'''
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
